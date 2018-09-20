777f1329c7e52944121b4a35d681af2d11403a1d
jphus <jphussey05@gmail.com>
2018-09-19 20:44:54 -0400

    first branch ch05 push

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from app import db
-
 
 
 class User(db.Model):
@@ -10,8 +10,15 @@ class User(db.Model):
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
